06a5f2023dbc2303c2840e20ccc8947f0c466a12
aidanohart <44532573+aidanohart@users.noreply.github.com>
2024-02-16 11:46:49 +0000

    Create xptop.js

---
+++ commands/fun/xptop.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { db_host, db_user, db_password, db_database } = require('../../config.json');
+const mysql = require('mysql2/promise'); // Import the promise-based version of mysql
+
+const pool = mysql.createPool({
+    host: db_host,
+    user: db_user,
+    password: db_password,
+    database: db_database
+});
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('xptop')
+        .setDescription('View the Top 10 users with the most Xp.'),
+    async execute(interaction) {
+        try {
+            // Fetch the top 10 users with the most coins from the database
+            const [rows] = await pool.execute('SELECT u.id, u.xp, COUNT(m.id) AS message_count FROM users u LEFT JOIN messages m ON u.id = m.user GROUP BY u.id ORDER BY u.xp DESC LIMIT 10');
+
+            // Format the data into an embedded message
+            const embed = new EmbedBuilder()
+                .setColor('#0099ff')
+                .setTitle('Top 10 Users with the Most Xp')
+                .setDescription(rows.map((row, index) => `**${index + 1}.** <@${row.id}> - ${row.xp.toLocaleString()} Xp (${row.message_count.toLocaleString()} Messages Sent)`).join('\n'));
+
+            // Send the embedded message
+            await interaction.reply({ embeds: [embed] });
+        } catch (error) {
+            console.error('Error executing cointop command:', error);
+            await interaction.reply('An error occurred while processing your command.');
+        }
+    },
+};
