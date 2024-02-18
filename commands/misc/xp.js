e1694927a38425687b124635c96eee1161063ec7
aidanohart <44532573+aidanohart@users.noreply.github.com>
2024-02-18 12:50:07 +0000

    Update xp.js

--- commands/misc/xp.js
+++ commands/misc/xp.js
@@ -24,14 +24,16 @@ module.exports = {
         const userId = userOption ? userOption.id : interaction.user.id;
 
         try {
-            const [rows] = await pool.execute('SELECT u.xp, COUNT(m.id) AS messageCount FROM users u LEFT JOIN messages m ON u.id = m.user WHERE u.id = ?', [userId]);
+            const [rows] = await pool.execute('SELECT u.id, u.xp, COUNT(m.id) AS messageCount, (SELECT AVG(xp) FROM messages WHERE user = u.id) AS averagexp FROM users u LEFT JOIN messages m ON u.id = m.user WHERE u.id = ?', [userId]);
 
             if (rows.length > 0) {
                 const xp = rows[0].xp;
-                const messages = rows[0].messageCount;
+                const messages = rows[0].messageCount;                                
+                const averagexp = parseFloat(rows[0].averagexp).toFixed(2);
+
                 const embed = new EmbedBuilder()
                     .setColor('#0099ff')
-                    .setDescription(`<@${userId}> has **${xp.toLocaleString()}** Xp and has sent **${messages.toLocaleString()}** messages. (Level System Coming Soon)`);
+                    .setDescription(`<@${userId}> has **${xp.toLocaleString()}** Xp and sent **${messages.toLocaleString()}** messages (${averagexp}Xp per message)`);
 
                 await interaction.reply({ embeds: [embed] });
             } else {
