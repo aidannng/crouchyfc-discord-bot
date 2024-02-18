8742c4f65d4dabd635dd797c89ab055dcbccab2d
aidanohart <44532573+aidanohart@users.noreply.github.com>
2024-02-18 13:08:51 +0000

    Update xptop.js

--- commands/fun/xptop.js
+++ commands/fun/xptop.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { db_host, db_user, db_password, db_database } = require('../../config.json');
-const mysql = require('mysql2/promise'); // Import the promise-based version of mysql
+const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
     host: db_host,
@@ -18,11 +18,14 @@ module.exports = {
             // Fetch the top 10 users with the most coins from the database
             const [rows] = await pool.execute('SELECT u.id, u.xp, COUNT(m.id) AS message_count FROM users u LEFT JOIN messages m ON u.id = m.user GROUP BY u.id ORDER BY u.xp DESC LIMIT 10');
 
-            // Format the data into an embedded message
+            // Format the data into an array of strings
+            const topUsers = rows.map((row, index) => `**${index + 1}.** <@${row.id}> - ${row.xp.toLocaleString()} Xp (${row.message_count.toLocaleString()} Messages Sent)`);
+
+            // Create an embedded message
             const embed = new EmbedBuilder()
                 .setColor('#0099ff')
                 .setTitle('Top 10 Users with the Most Xp')
-                .setDescription(rows.map((row, index) => `**${index + 1}.** <@${row.id}> - ${row.xp.toLocaleString()} Xp (${row.message_count.toLocaleString()} Messages Sent)`).join('\n'));
+                .setDescription(topUsers.join('\n'));
 
             // Send the embedded message
             await interaction.reply({ embeds: [embed] });
